5d81993c6d41383d6b425a9183aed1b62095f98a
cheandrey <skyandreymann@gmail.com>
2024-06-02 22:17:50 +0300

    witf_second_func

--- str_func.py
+++ str_func.py
@@ -2,3 +2,10 @@ def cap_lit(instring):
 
     """ функция для возвращения строки пользователя заглавными буквами """
     return instring.upper()
+
+def first_cap(instring):
+
+    """ функция для возврата строки пользователя с первой заглавной буквой"""
+    words = instring.split()
+    cap_words = [word.capitalize() for word in words]
+    return ''.join(cap_words)
