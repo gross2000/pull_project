232b8ec8161c40123447754db285a0fb6c95a628
cheandrey <skyandreymann@gmail.com>
2024-06-02 23:09:39 +0300

    with_second_func

--- str_func.py
+++ str_func.py
@@ -1,14 +1,7 @@
 def cap_lit(instring):
 
-    """ функция для возвращения строки пользователя заглавными буквами """
+    """ функция для возвращения строки пользователя заглавными буквами, строка для конфликта """
     return instring.upper()
-
-def first_cap(instring):
-
-    """ функция для возврата строки пользователя с первой заглавной буквой"""
-    words = instring.split()
-    cap_words = [word.capitalize() for word in words]
-    return ''.join(cap_words)
 
 def first_cap(instring):
 
